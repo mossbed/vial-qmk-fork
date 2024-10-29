ceaef36b17b07bf2fa5155816786f60301e6cf5f
boa <rubberboa01@gmail.com>
2024-10-29 03:30:50 -0700

    fix vial.json key matrix

--- keyboards/rk/r75/iamdanielv/keymaps/via/features/indicators.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include QMK_KEYBOARD_H
-
-void blink_arrows(void);
-void blink_space(bool);
-void blink_NKRO(bool);
-void blink_numbers(bool);
-void highlight_fn_keys(uint8_t led_min, uint8_t led_max);
--- keyboards/rk/r75/iamdanielv/keymaps/vial/config.h
+++ keyboards/rk/r75/iamdanielv/keymaps/vial/config.h
@@ -2,5 +2,5 @@
 
 #define VIAL_KEYBOARD_UID {0x99, 0x44, 0xA9, 0xE0, 0x01, 0x96, 0x62, 0x72}
 
-// #define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
-// #define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 12}
